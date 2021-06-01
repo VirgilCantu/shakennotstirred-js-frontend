a96a8bd6582a085a91c5317273fa954bad92ab08
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-01 14:42:15 -0400

    created getCocktails method in CocktailsAdapter class, which fetches using a GET request to cocktailUrl

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ class CocktailsAdapter {
   constructor() {
     this.cocktailUrl = "http://localhost:3000/cocktails";
   }
+
+  getCocktails() {
+    return fetch(this.cocktailUrl).then(res => res.json());
+  }
 }
