e109c0460e473b9e067c29bada4aac875b32713e
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-01 15:41:25 -0400

    created createCocktail method in CocktailsAdapter class, which fetches using a POST request to cocktailUrl, and creates a new Cocktail object

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ class CocktailsAdapter {
   getCocktails() {
     return fetch(this.cocktailUrl).then(res => res.json());
   }
+
+  createCocktail(body) {
+    const cocktailCreateParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(body)
+    };
+    return fetch(this.cocktailUrl, cocktailCreateParams).then(res => res.json());
+  }
 }
