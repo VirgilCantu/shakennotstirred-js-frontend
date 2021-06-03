c4f20d4e109e770f5567e618cc3e1052d29c99ac
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-03 03:05:41 -0400

    created cocktailsAdapter.js and migrated CocktailsAdapter class into file

---
+++ cocktailsAdapter.js
@@ -0,0 +1,40 @@
+class CocktailsAdapter {
+  constructor() {
+    this.cocktailUrl = "http://localhost:3000/cocktails";
+  }
+
+  getCocktails() {
+    return fetch(this.cocktailUrl).then(res => res.json());
+  }
+
+  createCocktail(body) {
+    const cocktailCreateParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(body)
+    };
+    return fetch(this.cocktailUrl, cocktailCreateParams).then(res => res.json());
+  }
+
+  editCocktail(body, cocktailId) {
+    const cocktailEditParams = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(body)
+    };
+    return fetch(`${this.cocktailUrl}/${cocktailId}`, cocktailEditParams).then(res => res.json());
+  }
+
+  deleteCocktail(cocktailId) {
+    const cocktailDeleteParams = {
+      method: "DELETE"
+    };
+    return fetch(`${this.cocktailUrl}/${cocktailId}`, cocktailDeleteParams);
+  }
+}
--- index.js
+++ index.js
@@ -1,73 +1,32 @@
-class CocktailsAdapter {
-  constructor() {
-    this.cocktailUrl = "http://localhost:3000/cocktails";
-  }
+//   createCocktailIndex() {
+//     const div = document.createElement("div");
+//     div.id = `cocktail-${cocktail.id}`;
+//     cocktails.appendChild(div);
 
-  getCocktails() {
-    return fetch(this.cocktailUrl).then(res => res.json());
-  }
+//     const image = document.createElement("img");
+//     image.src = cocktail.image;
+//     image.className = "cocktail_img";
+//     div.appendChild(image);
 
-  createCocktail(body) {
-    const cocktailCreateParams = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(body)
-    };
-    return fetch(this.cocktailUrl, cocktailCreateParams).then(res => res.json());
-  }
+//     const name = document.createElement("h3");
+//     name.innerHTML = `<strong>${cocktail.name}</strong>`;
+//     div.appendChild(name);
 
-  editCocktail(body, cocktailId) {
-    const cocktailEditParams = {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(body)
-    };
-    return fetch(`${this.cocktailUrl}/${cocktailId}`, cocktailEditParams).then(res => res.json());
-  }
+//     const glassware = document.createElement("p");
+//     glassware.innerHTML = `Glassware: ${cocktail.glassware}`;
+//     div.appendChild(glassware);
 
-  deleteCocktail(cocktailId) {
-    const cocktailDeleteParams = {
-      method: "DELETE"
-    };
-    return fetch(`${this.cocktailUrl}/${cocktailId}`, cocktailDeleteParams);
-  }
+//     const ice = document.createElement("p");
+//     ice.innerHTML = `Ice: ${cocktail.ice}`;
+//     div.appendChild(ice);
 
-  //   createCocktailIndex() {
-  //     const div = document.createElement("div");
-  //     div.id = `cocktail-${cocktail.id}`;
-  //     cocktails.appendChild(div);
+//     const origin = document.createElement("p");
+//     origin.innerHTML = `Origin: ${cocktail.origin}`;
+//     div.appendChild(origin);
 
-  //     const image = document.createElement("img");
-  //     image.src = cocktail.image;
-  //     image.className = "cocktail_img";
-  //     div.appendChild(image);
-
-  //     const name = document.createElement("h3");
-  //     name.innerHTML = `<strong>${cocktail.name}</strong>`;
-  //     div.appendChild(name);
-
-  //     const glassware = document.createElement("p");
-  //     glassware.innerHTML = `Glassware: ${cocktail.glassware}`;
-  //     div.appendChild(glassware);
-
-  //     const ice = document.createElement("p");
-  //     ice.innerHTML = `Ice: ${cocktail.ice}`;
-  //     div.appendChild(ice);
-
-  //     const origin = document.createElement("p");
-  //     origin.innerHTML = `Origin: ${cocktail.origin}`;
-  //     div.appendChild(origin);
-
-  //     const prep = document.createElement("p");
-  //     prep.innerHTML = `Preparation: ${cocktail.preparation}`;
-  //     div.appendChild(prep);
-  //   }
-}
+//     const prep = document.createElement("p");
+//     prep.innerHTML = `Preparation: ${cocktail.preparation}`;
+//     div.appendChild(prep);
+//   }
 
 const app = new App();
