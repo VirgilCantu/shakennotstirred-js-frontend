e30e12412ef6b51055b3133d455e64771821343a
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-03 02:17:29 -0400

    separated logic of fetchAndLoadCocktails into two separate methods, one for putting fetched objects into an array for later use and the other for making individual cocktail cards to put into the DOM

--- index.js
+++ index.js
@@ -41,46 +41,53 @@ class CocktailsAdapter {
 
 class Cocktails {
   constructor() {
-    // this.cocktails = [];
-    // this.eventListeners();
     this.adapter = new CocktailsAdapter();
     this.fetchAndLoadCocktails();
+    this.createCocktailIndex();
   }
 
   fetchAndLoadCocktails() {
+    this.cocktailList = [];
+
     this.adapter.getCocktails().then(json => {
-      const cocktails = document.getElementById("current-cocktails");
-      cocktails.className = "cocktail_div";
       json.forEach(obj => {
-        const div = document.createElement("div");
-        div.id = `cocktail-${obj.id}`;
-        cocktails.appendChild(div);
-
-        const image = document.createElement("img");
-        image.src = obj.image;
-        image.className = "cocktail_img";
-        div.appendChild(image);
-
-        const name = document.createElement("h3");
-        name.innerHTML = `<strong>${obj.name}</strong>`;
-        div.appendChild(name);
-
-        const glassware = document.createElement("p");
-        glassware.innerHTML = `Glassware: ${obj.glassware}`;
-        div.appendChild(glassware);
-
-        const ice = document.createElement("p");
-        ice.innerHTML = `Ice: ${obj.ice}`;
-        div.appendChild(ice);
-
-        const origin = document.createElement("p");
-        origin.innerHTML = `Origin: ${obj.origin}`;
-        div.appendChild(origin);
-
-        const prep = document.createElement("p");
-        prep.innerHTML = `Preparation: ${obj.preparation}`;
-        div.appendChild(prep);
+        this.cocktailList.push(obj);
       });
+    });
+  }
+
+  createCocktailIndex() {
+    const cocktails = document.getElementById("current-cocktails");
+    cocktails.className = "cocktail_div";
+    this.cocktailList.forEach(cocktail => {
+      const div = document.createElement("div");
+      div.id = `cocktail-${cocktail.id}`;
+      cocktails.appendChild(div);
+
+      const image = document.createElement("img");
+      image.src = cocktail.image;
+      image.className = "cocktail_img";
+      div.appendChild(image);
+
+      const name = document.createElement("h3");
+      name.innerHTML = `<strong>${cocktail.name}</strong>`;
+      div.appendChild(name);
+
+      const glassware = document.createElement("p");
+      glassware.innerHTML = `Glassware: ${cocktail.glassware}`;
+      div.appendChild(glassware);
+
+      const ice = document.createElement("p");
+      ice.innerHTML = `Ice: ${cocktail.ice}`;
+      div.appendChild(ice);
+
+      const origin = document.createElement("p");
+      origin.innerHTML = `Origin: ${cocktail.origin}`;
+      div.appendChild(origin);
+
+      const prep = document.createElement("p");
+      prep.innerHTML = `Preparation: ${cocktail.preparation}`;
+      div.appendChild(prep);
     });
   }
 }
