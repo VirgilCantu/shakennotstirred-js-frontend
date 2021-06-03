5812e9030d7daa7e1292ddf1341455009c8d4e25
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-03 02:44:36 -0400

    commented out createCocktailIndex method in Cocktails class, will be transferred to and reconfigured to work with Cocktail class, separation of concerns

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class CocktailsAdapter {
 
 class Cocktails {
   constructor() {
-    this.cocktailList = [];
+    this.cocktails = [];
     this.initBindingsAndEventListeners();
     this.adapter = new CocktailsAdapter();
     this.fetchAndLoadCocktails();
@@ -52,42 +52,39 @@ class Cocktails {
   }
 
   fetchAndLoadCocktails() {
-    this.adapter.getCocktails().then(cocktailsJSON => cocktailsJSON.forEach(cocktail => this.cocktailList.push(new Cocktail(cocktail))));
+    this.adapter.getCocktails().then(cocktailsJSON => cocktailsJSON.forEach(cocktail => this.cocktails.push(new Cocktail(cocktail))));
   }
 
-  createCocktailIndex() {
-    cocktails.className = "cocktail_div";
-    this.cocktailList.forEach(cocktail => {
-      const div = document.createElement("div");
-      div.id = `cocktail-${cocktail.id}`;
-      cocktails.appendChild(div);
+  //   createCocktailIndex() {
+  //     const div = document.createElement("div");
+  //     div.id = `cocktail-${cocktail.id}`;
+  //     cocktails.appendChild(div);
 
-      const image = document.createElement("img");
-      image.src = cocktail.image;
-      image.className = "cocktail_img";
-      div.appendChild(image);
+  //     const image = document.createElement("img");
+  //     image.src = cocktail.image;
+  //     image.className = "cocktail_img";
+  //     div.appendChild(image);
 
-      const name = document.createElement("h3");
-      name.innerHTML = `<strong>${cocktail.name}</strong>`;
-      div.appendChild(name);
+  //     const name = document.createElement("h3");
+  //     name.innerHTML = `<strong>${cocktail.name}</strong>`;
+  //     div.appendChild(name);
 
-      const glassware = document.createElement("p");
-      glassware.innerHTML = `Glassware: ${cocktail.glassware}`;
-      div.appendChild(glassware);
+  //     const glassware = document.createElement("p");
+  //     glassware.innerHTML = `Glassware: ${cocktail.glassware}`;
+  //     div.appendChild(glassware);
 
-      const ice = document.createElement("p");
-      ice.innerHTML = `Ice: ${cocktail.ice}`;
-      div.appendChild(ice);
+  //     const ice = document.createElement("p");
+  //     ice.innerHTML = `Ice: ${cocktail.ice}`;
+  //     div.appendChild(ice);
 
-      const origin = document.createElement("p");
-      origin.innerHTML = `Origin: ${cocktail.origin}`;
-      div.appendChild(origin);
+  //     const origin = document.createElement("p");
+  //     origin.innerHTML = `Origin: ${cocktail.origin}`;
+  //     div.appendChild(origin);
 
-      const prep = document.createElement("p");
-      prep.innerHTML = `Preparation: ${cocktail.preparation}`;
-      div.appendChild(prep);
-    });
-  }
+  //     const prep = document.createElement("p");
+  //     prep.innerHTML = `Preparation: ${cocktail.preparation}`;
+  //     div.appendChild(prep);
+  //   }
 }
 
 class Cocktail {
