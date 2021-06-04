9383d4dacc9fbe1e24e4a3d60c9a552bc30496af
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-04 17:55:03 -0400

    added event listener to createButton within importantNodes method in Ingredients class

--- src/components/ingredients.js
+++ src/components/ingredients.js
@@ -7,5 +7,6 @@ class Ingredients {
     this.ingredientsForm = document.getElementById("new-ingredient-form");
     this.createButton = document.getElementById("create-ingredient-button");
     this.ingredientsForm.style.display = "none";
+    this.createButton.addEventListener("click", this.showHideForm);
   }
 }
