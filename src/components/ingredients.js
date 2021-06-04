0aa1328c00341a8e9cc720c10db8fd42f9a18284
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-04 18:02:10 -0400

    added showHideForm method to serve as callback to createButton event listener in Ingredients class, also added cocktailsForm property to importantNodes method in same class

--- src/components/ingredients.js
+++ src/components/ingredients.js
@@ -5,8 +5,19 @@ class Ingredients {
 
   importantNodes() {
     this.ingredientsForm = document.getElementById("new-ingredient-form");
+    this.cocktailsForm = document.getElementById("new-cocktail-form");
     this.createButton = document.getElementById("create-ingredient-button");
     this.ingredientsForm.style.display = "none";
     this.createButton.addEventListener("click", this.showHideForm);
   }
+
+  showHideForm = () => {
+    if (this.ingredientsForm.style.display === "none") {
+      this.ingredientsForm.style.display = "block";
+      this.createButton.innerHTML = "Close";
+    } else {
+      this.ingredientsForm.style.display = "none";
+      this.createButton.innerHTML = "Add Ingredient";
+    }
+  };
 }
